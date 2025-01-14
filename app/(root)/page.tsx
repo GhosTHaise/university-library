dad621b5db9aca8d039ad02dbe90da4f9f6d5f1a
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-01-14 20:08:52 +0300

    feat : show book cover

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import BookList from "@/components/bookList";
 import BookOverview from "@/components/bookOverview";
+import { sampleBooks } from "@/constants";
 
 const Home = () => {
   return (
     <>
-      <BookOverview />
-      <BookList />
+      <BookOverview
+        {...sampleBooks[0]}
+      />
+      <BookList 
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 }
