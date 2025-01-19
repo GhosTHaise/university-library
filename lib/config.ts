af3ce4182bd859a99fabb51737aa6e328e7d73c4
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-01-19 22:44:22 +0300

    feat : create authentificator function (50% done)

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,10 @@
 const config = {
     env : {
+        apiEndpoint : process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imagekit : {
-            publicKey : process.env.NEXT_PUBLIC_IMAGEKIT_PUBBLIC_KEY,
-            privateKey : process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY,
-            urlEndpoint : process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT
+            publicKey : process.env.NEXT_PUBLIC_IMAGEKIT_PUBBLIC_KEY!,
+            privateKey : process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY!,
+            urlEndpoint : process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!
         }
     }
 }
