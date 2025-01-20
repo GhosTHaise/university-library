b2467f8263fa628576f123163c8a956e10a3a4ac
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-01-20 22:33:52 +0300

    feat : upload file front

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -1,5 +1,6 @@
 import config from '@/lib/config';
 import ImageKit from 'imagekit'
+import { NextResponse } from 'next/server';
 
 const { env : { imagekit: { publicKey, privateKey, urlEndpoint } } } = config;
 
@@ -8,3 +9,8 @@ const imagekit = new ImageKit({
     privateKey: privateKey,
     urlEndpoint: urlEndpoint,
 });
+
+
+export async function GET() {
+    return NextResponse.json(imagekit.getAuthenticationParameters())
+}
