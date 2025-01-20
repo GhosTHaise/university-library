1840ffa6fe993efcad915a31380c6c6a255f1b26
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-01-20 22:21:02 +0300

    feat : setup imagekit

---
+++ app/api/auth/imagekit/route.ts
@@ -0,0 +1,10 @@
+import config from '@/lib/config';
+import ImageKit from 'imagekit'
+
+const { env : { imagekit: { publicKey, privateKey, urlEndpoint } } } = config;
+
+const imagekit = new ImageKit({
+    publicKey: publicKey,
+    privateKey: privateKey,
+    urlEndpoint: urlEndpoint,
+});
